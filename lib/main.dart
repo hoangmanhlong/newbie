d00d3a81ba821dad2b28dfa05ae518860b2bae37
LongHM <hoang.manh.long@zensho.com>
2024-10-02 16:24:55 +0700

    test: create test app using riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// To install Riverpod, we need to add this widget above everything else.
+// This should not be inside "MyApp" but as direct parameter to "runApp".
+void main() => runApp(const ProviderScope(child: MyApp()));
+
+final tasksProvider = StateNotifierProvider<TaskNotifier, List<Task>>((ref) {
+  return TaskNotifier(tasks: [
+    Task(id: 1, label: 'Load rocket with supplies'),
+    Task(id: 2, label: 'Launch rocket'),
+    Task(id: 3, label: 'Circle the home planet'),
+    Task(id: 4, label: 'Head out to the first moon'),
+    Task(id: 5, label: 'Launch moon lander #1')
+  ]);
+});
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -31,95 +42,88 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('App')),
+      body: Column(
+        children: [
+          AppProgressBar(),
+          ListTask()
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class AppProgressBar extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(tasksProvider);
+    var numCompletedTasks  = tasks.where((task) {return task.checked == true;}).length;
+    return LinearProgressIndicator(value: numCompletedTasks / tasks.length);
+  }
+}
+
+class ListTask extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var tasks = ref.watch(tasksProvider);
+    return Column(children: tasks.map((task) => TaskItem(task: task)).toList());
+  }
+}
+
+class TaskItem extends ConsumerWidget {
+  final Task task;
+
+  TaskItem({Key? key, required this.task}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(children: [
+        Checkbox(
+            value: task.checked,
+            onChanged: (checkedState) =>
+                ref.read(tasksProvider.notifier).toggle(task.id)
+        ),
+        SizedBox(width: 16),
+        Text(task.label)
+      ]),
+    );
+  }
+}
+
+class Task {
+  final int id;
+  final String label;
+  final bool checked;
+
+  Task({required this.id, required this.label, this.checked = false});
+
+  Task updateCheckedStatus(bool checked) =>
+      Task(id: id, label: label, checked: checked);
+}
+
+class TaskNotifier extends StateNotifier<List<Task>> {
+  TaskNotifier({tasks}) : super(tasks);
+
+  void add(Task task) {
+    state = [...state, task];
+  }
+
+  void toggle(int taskId) {
+    state = [
+      for (final item in state)
+        if (item.id == taskId) item.updateCheckedStatus(!item.checked) else item
+    ];
   }
 }
